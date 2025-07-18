edef849db63c57c42477e886cd7150dfb13654da
Yahya <yahya6789@gmail.com>
2025-07-18 11:02:43 +0700

    docs: add JavaDoc remarks to TodoRel class

--- src/main/java/com/github/yahya6789/todo/application/api/hateoas/TodoRel.java
+++ src/main/java/com/github/yahya6789/todo/application/api/hateoas/TodoRel.java
@@ -1,11 +1,21 @@
 package com.github.yahya6789.todo.application.api.hateoas;
 
+/**
+ * HATEOAS relation names for Todo API endpoints.
+ */
 public final class TodoRel {
+	/** Relation name for all collection */
 	public static final String TODOS = "todos";
-	public static final String TOGGLE_COMPLETED = "toggle-completed";
+
+	/** Relation name for update operation */
 	public static final String UPDATE = "update";
+
+	/** Relation name for delete operation */
 	public static final String DELETE = "delete";
 
+	/** Relation name for toggle completion operation */
+	public static final String TOGGLE_COMPLETED = "toggle-completed";
+
 	private TodoRel() {
-	} // Prevent instantiation
+	}
 }
