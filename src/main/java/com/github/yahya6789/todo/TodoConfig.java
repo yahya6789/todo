cfe63a9dbc6f1e379aebcf6b9087b28f5fc42cea
Yahya <yahya6789@gmail.com>
2025-07-18 09:42:00 +0700

    chore: add messages.properties template

--- src/main/java/com/github/yahya6789/todo/TodoConfig.java
+++ src/main/java/com/github/yahya6789/todo/TodoConfig.java
@@ -1,10 +1,19 @@
 package com.github.yahya6789.todo;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
 public class TodoConfig {
-	// You can also define shared beans here in the future
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:messages/messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
 }
